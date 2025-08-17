fd3abdbf6772ee7910c98dd201d5d30858d6449d
Jeyong <dlwpdyd201@naver.com>
2025-08-17 23:55:19 +0900

    [feat] 인증 및 인가 관련 예외 코드 작성

--- src/main/java/dev/woori/wooriLog/global/response/error/ErrorBaseCode.java
+++ src/main/java/dev/woori/wooriLog/global/response/error/ErrorBaseCode.java
@@ -10,9 +10,26 @@ import org.springframework.http.HttpStatus;
 public enum ErrorBaseCode implements ErrorCode {
 
     /**
+     * 400 - BAD_REQUEST
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, 400, "잘못된 요청입니다."),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, 400, "토큰이 유효하지 않습니다."),
+
+    /**
+     * 401 - UNAUTHORIZED
+     */
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 401, "토큰이 만료되었습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 401, "인증되지 않은 사용자입니다."),
+
+    /**
      * 404 NOT FOUND
      */
-    NOT_FOUND_API(HttpStatus.NOT_FOUND, 404, "잘못된 API 요청입니다.");
+    NOT_FOUND_API(HttpStatus.NOT_FOUND, 404, "잘못된 API 요청입니다."),
+
+    /**
+     * 500
+     */
+    URL_DECODE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "URL 디코딩 에러");
 
     private final HttpStatus httpStatus;
     private final int code;
