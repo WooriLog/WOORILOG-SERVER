2ff1f4adf0fbb82ca54ed63ac0407b4609bac20d
Jeyong <dlwpdyd201@naver.com>
2025-08-17 23:55:03 +0900

    [feat] 인증 및 인가 관련 예외 및 JWT 필터

---
+++ src/main/java/dev/woori/wooriLog/global/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,51 @@
+package dev.woori.wooriLog.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.woori.wooriLog.global.auth.Constants;
+import dev.woori.wooriLog.global.exception.JwtTokenException;
+import dev.woori.wooriLog.global.response.BaseResponse;
+import dev.woori.wooriLog.global.response.error.ErrorBaseCode;
+import dev.woori.wooriLog.global.response.error.ErrorCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            handleUnauthorizedException(response, e);
+        }
+    }
+
+    private void handleUnauthorizedException(HttpServletResponse response, Exception e) throws IOException {
+        JwtTokenException jwtException = (JwtTokenException) e;
+        ErrorCode errorCode = jwtException.getErrorCode();
+        HttpStatus httpStatus = errorCode.getHttpStatus();
+        setResponse(response, httpStatus, errorCode);
+    }
+
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        setResponse(response, HttpStatus.INTERNAL_SERVER_ERROR, ErrorBaseCode.BAD_REQUEST);
+    }
+
+    private void setResponse(HttpServletResponse response, HttpStatus httpStatus, ErrorCode errorBaseCode) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(Constants.CHARACTER_TYPE);
+        response.setStatus(httpStatus.value());
+        PrintWriter writer = response.getWriter();
+        writer.write(objectMapper.writeValueAsString(BaseResponse.of(errorBaseCode)));
+    }
+}
+
---
+++ src/main/java/dev/woori/wooriLog/global/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package dev.woori.wooriLog.global.filter;
+
+import dev.woori.wooriLog.global.auth.Constants;
+import dev.woori.wooriLog.global.auth.jwt.JwtProvider;
+import dev.woori.wooriLog.global.auth.jwt.TokenAuthentication;
+import dev.woori.wooriLog.global.exception.JwtTokenException;
+import dev.woori.wooriLog.global.response.error.ErrorBaseCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static dev.woori.wooriLog.global.auth.jwt.TokenAuthentication.createTokenAuthentication;
+
+/**
+ * SpringSecurity FilterChain에 추가할 JWT 관련 Filter
+ */
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String accessToken = getAccessToken(request);
+
+        final long userId = jwtProvider.getUserIdFromClaims(accessToken);
+
+        // Token을 이용한 인증 객체 생성
+        doAuthentication(accessToken, userId);
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(final HttpServletRequest request) {
+        final String accessToken = request.getHeader(Constants.AUTHORIZATION);
+        if (StringUtils.hasText(accessToken) && accessToken.startsWith(Constants.BEARER)) {
+            // Bearer를 제외한 순수 aT
+            return accessToken.substring(Constants.BEARER.length());
+        }
+        throw new JwtTokenException(ErrorBaseCode.UNAUTHORIZED);
+    }
+
+    private void doAuthentication(final String token, final long userId) {
+        TokenAuthentication tokenAuthentication = createTokenAuthentication(token, userId);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(tokenAuthentication);
+    }
+}
