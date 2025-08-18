0422732d3a236b97a3b747a0a354cd5ac7ba5647
Jeyong <dlwpdyd201@naver.com>
2025-08-18 09:00:05 +0900

    [refactor] 파라미터 변수명 변경

--- src/main/java/dev/woori/wooriLog/domain/member/repository/MemberRepository.java
+++ src/main/java/dev/woori/wooriLog/domain/member/repository/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByProviderAndSocialId(String google, String sub);
+    Optional<Member> findByProviderAndSocialId(String provider, String socialId);
 }
