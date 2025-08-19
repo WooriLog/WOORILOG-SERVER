aff2bd95594213abf2e1e48234ad248dc25e9fa8
Jeyong <dlwpdyd201@naver.com>
2025-08-20 00:31:07 +0900

    [feat] 커스텀 애노테이션 @UserId 구현

---
+++ src/main/java/dev/woori/wooriLog/global/resolver/UserId.java
@@ -0,0 +1,8 @@
+package dev.woori.wooriLog.global.resolver;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface UserId {}
