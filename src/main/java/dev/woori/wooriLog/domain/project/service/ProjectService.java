2f0f7767a3eee30d0cd3961f53a69e5008a099d6
Jeyong <dlwpdyd201@naver.com>
2025-08-28 00:24:15 +0900

    [refactor] 코드 리뷰 적용 - N+1 문제 해결을 위한 리팩토링

--- src/main/java/dev/woori/wooriLog/domain/project/service/ProjectService.java
+++ src/main/java/dev/woori/wooriLog/domain/project/service/ProjectService.java
@@ -19,7 +19,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static dev.woori.wooriLog.domain.DomainConstants.LEADER;
 
@@ -33,6 +36,11 @@ public class ProjectService {
     private final MemberRepository memberRepository;
     private final BlogRepository blogRepository;
 
+    /**
+     * 프로젝트 생성 메서드
+     * @param leaderId 팀장의 memberId
+     * @param request 프로젝트 생성 요청 DTO
+     */
     @Transactional
     public void createProject(Long leaderId, ProjectCreateReq request) {
         // 프로젝트 생성
@@ -46,6 +54,11 @@ public class ProjectService {
         addMembersToProject(request, project);
     }
 
+    /**
+     * 프로젝트 정보 조회 메서드
+     * @param projectId 프로젝트 Id
+     * @return 프로젝트 정보(기본 정보, 멤버 정보, 포스팅 정보) DTO
+     */
     @Transactional
     public ProjectInfoRes getProjectInfo(Long projectId) {
         Project project = findProjectBy(projectId);
@@ -74,13 +87,28 @@ public class ProjectService {
 
     private void addMembersToProject(ProjectCreateReq request, Project project) {
         List<ProjectMemberDto> members = request.members();
+        if (members == null || members.isEmpty()) {
+            return;
+        }
+
+        List<Long> memberIds = members.stream()
+                .filter(Objects::nonNull)
+                .map(ProjectMemberDto::userId)
+                .toList();
+
+        // N+1 문제 개선을 위해 전체 멤버 조회 후 Map으로 저장하여 Map에서 필터링 및 DTO 변환 -> O(1)으로 유저 탐색 가능
+        Map<Long, Member> memberMap = memberRepository.findAllById(memberIds).stream()
+                .collect(Collectors.toMap(Member::getId, Function.identity()));
 
         List<ProjectMember> relations = members.stream()
-                        .filter(Objects::nonNull)
-                                .map(memberInfo -> {
-                                    Member member = findMemberBy(memberInfo.userId(), memberInfo.email());
-                                    return ProjectMember.of(member, project, memberInfo.role());
-                                }).toList();
+                .filter(Objects::nonNull)
+                .map(memberInfo -> {
+                    Member member = memberMap.get(memberInfo.userId());
+                    if (member == null || !member.getEmail().equals(memberInfo.email())) {
+                        throw new IllegalArgumentException();
+                    }
+                    return ProjectMember.of(member, project, memberInfo.role());
+                }).toList();
 
         projectMemberRepository.saveAll(relations);
     }
