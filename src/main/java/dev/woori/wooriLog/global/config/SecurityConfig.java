7fc4870b3cfe2d2e064a8e30aee08cb72556b9c3
Jeyong <dlwpdyd201@naver.com>
2025-09-03 09:40:03 +0900

    [hotfix] 시연을 위한 엔드포인트 임시 개방 (수정)

--- src/main/java/dev/woori/wooriLog/global/config/SecurityConfig.java
+++ src/main/java/dev/woori/wooriLog/global/config/SecurityConfig.java
@@ -31,7 +31,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
                 "/api/google/enroll",
                 "/api/projects/home",
                 "/api/blog/home",
-                "/api/blogs/**", // 시연을 위한 임시 개방
+                "/api/blog/**", // 시연을 위한 임시 개방
                 "/api/members/**", // 시연을 위한 임시 개방
                 "/api/projects/**" // 시연을 위한 임시 개방
         );
