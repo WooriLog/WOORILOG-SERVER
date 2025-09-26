f377e2d3a779d3f98d6045f53666f9e2581a8be0
Jeyong <dlwpdyd201@naver.com>
2025-09-26 23:07:24 +0900

    [chroe] API 설명 주석 추가

--- src/main/java/dev/woori/wooriLog/domain/project/controller/ProjectController.java
+++ src/main/java/dev/woori/wooriLog/domain/project/controller/ProjectController.java
@@ -19,11 +19,13 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
+    // 최신 5개의 프로젝트 조회 (홈화면)
     @GetMapping("/projects/home")
     public ResponseEntity<BaseResponse<?>> getHomeProjectInfos() {
         return ApiResponseUtil.success(SuccessCode.OK, projectService.getProjectBasicInfos());
     }
 
+    // 프로젝트 생성
     @PostMapping("/projects")
     public ResponseEntity<BaseResponse<?>> createProject(
             @UserId Long leaderId,
@@ -35,11 +37,13 @@ public class ProjectController {
         );
     }
 
+    // 프로젝트 조회
     @GetMapping("/projects/{projectId}")
     public ResponseEntity<BaseResponse<?>> getProjectInfo(@PathVariable Long projectId) {
         return ApiResponseUtil.success(SuccessCode.OK, projectService.getProjectInfo(projectId));
     }
 
+    // 프로젝트 가입 목록 조회 (유저)
     @GetMapping("/projects/list")
     public ResponseEntity<BaseResponse<?>> getProjectList(@UserId Long memberId) {
         return ApiResponseUtil.success(SuccessCode.OK, projectService.getProjectListByMemberId(memberId));
