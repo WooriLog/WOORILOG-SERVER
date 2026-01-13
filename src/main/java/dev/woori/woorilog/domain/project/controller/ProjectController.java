71cebc98c5d3883bee40a8637eb5877c39d6efeb
Jeyong <dlwpdyd201@naver.com>
2026-01-13 13:48:54 +0900

    [refactor] wildcard 제거 및 ResponseEntity 응답 객체 명시

--- src/main/java/dev/woori/woorilog/domain/project/controller/ProjectController.java
+++ src/main/java/dev/woori/woorilog/domain/project/controller/ProjectController.java
@@ -1,7 +1,10 @@
 package dev.woori.woorilog.domain.project.controller;
 
+import dev.woori.woorilog.domain.project.dto.ProjectDetailInfoDto;
 import dev.woori.woorilog.domain.project.dto.request.ProjectCreateReq;
 import dev.woori.woorilog.domain.project.dto.response.ProjectCreateRes;
+import dev.woori.woorilog.domain.project.dto.response.ProjectHomeRes;
+import dev.woori.woorilog.domain.project.dto.response.ProjectInfoRes;
 import dev.woori.woorilog.domain.project.service.ProjectService;
 import dev.woori.woorilog.global.resolver.UserId;
 import dev.woori.woorilog.global.response.ApiResponseUtil;
@@ -12,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/projects")
@@ -21,13 +26,13 @@ public class ProjectController {
 
     // 최신 5개의 프로젝트 조회 (홈화면)
     @GetMapping("/home")
-    public ResponseEntity<BaseResponse<?>> getHomeProjectInfos(@RequestParam(defaultValue = "1") int page) {
+    public ResponseEntity<BaseResponse<ProjectHomeRes>> getHomeProjectInfos(@RequestParam(defaultValue = "1") int page) {
         return ApiResponseUtil.success(SuccessCode.OK, projectService.getProjectBasicInfos(page));
     }
 
     // 프로젝트 생성
     @PostMapping
-    public ResponseEntity<BaseResponse<?>> createProject(
+    public ResponseEntity<BaseResponse<ProjectCreateRes>> createProject(
             @UserId Long leaderId,
             @Valid @RequestBody ProjectCreateReq request
     ) {
@@ -39,26 +44,26 @@ public class ProjectController {
 
     // 프로젝트 조회
     @GetMapping("/{projectId}")
-    public ResponseEntity<BaseResponse<?>> getProjectInfo(@PathVariable Long projectId) {
+    public ResponseEntity<BaseResponse<ProjectInfoRes>> getProjectInfo(@PathVariable Long projectId) {
         return ApiResponseUtil.success(SuccessCode.OK, projectService.getProjectInfo(projectId));
     }
 
     // 프로젝트 가입 목록 조회 (유저)
     @GetMapping("/list")
-    public ResponseEntity<BaseResponse<?>> getProjectList(@UserId Long memberId) {
+    public ResponseEntity<BaseResponse<List<ProjectDetailInfoDto>>> getProjectList(@UserId Long memberId) {
         return ApiResponseUtil.success(SuccessCode.OK, projectService.getProjectListByMemberId(memberId));
     }
 
     // 프로젝트 삭제
     @DeleteMapping("/{projectId}")
-    public ResponseEntity<BaseResponse<?>> deleteProject(@PathVariable Long projectId) {
+    public ResponseEntity<BaseResponse<Void>> deleteProject(@PathVariable Long projectId) {
         projectService.deleteProject(projectId);
         return ApiResponseUtil.success(SuccessCode.OK);
     }
 
     // 프로젝트 멤버 추가
     @PostMapping("/{projectId}/members/{memberId}")
-    public ResponseEntity<BaseResponse<?>> addProjectMember(
+    public ResponseEntity<BaseResponse<Void>> addProjectMember(
             @PathVariable Long projectId,
             @PathVariable Long memberId
     ) {
@@ -68,7 +73,7 @@ public class ProjectController {
 
     // 프로젝트 멤버 삭제
     @DeleteMapping("/{projectId}/members/{memberId}")
-    public ResponseEntity<BaseResponse<?>> deleteProjectMember(
+    public ResponseEntity<BaseResponse<Void>> deleteProjectMember(
             @PathVariable Long projectId,
             @UserId Long leaderId,
             @PathVariable Long memberId
